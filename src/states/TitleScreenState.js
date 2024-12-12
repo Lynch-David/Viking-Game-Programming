c6e5aff40615b939f0003f42f3b911db7ba2b50b
Lucas Lalumière <lucas.lalum@gmail.com>
2024-12-12 00:06:42 -0500

    Improved titlescreen UI

--- src/states/TitleScreenState.js
+++ src/states/TitleScreenState.js
@@ -19,6 +19,14 @@ export default class TitleScreenState extends State {
         this.currentSelection = 0;
         this.blinking = false;
         this.blinkState = true;
+        this.titleColor = { r: 255, g: 0, b: 0 }; // Initial color (red)
+        this.targetColor = { r: 0, g: 0, b: 255 }; // Target color (blue)
+        this.colorTransitionSpeed = 0.01; // Speed of color transition
+
+        // Set up a timer to toggle the target color
+        setInterval(() => {
+            this.targetColor = this.targetColor.r === 0 ? { r: 255, g: 0, b: 0 } : { r: 0, g: 0, b: 255 };
+        }, 3000); // Change target color every 3 seconds
     }
 
     enter() {
@@ -80,33 +88,52 @@ export default class TitleScreenState extends State {
         }
     }
 
+    lerpColor(start, end, t) {
+        return {
+            r: start.r + (end.r - start.r) * t,
+            g: start.g + (end.g - start.g) * t,
+            b: start.b + (end.b - start.b) * t,
+        };
+    }
+
     render() {
         context.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
         context.fillStyle = 'black';
         context.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
 
         context.strokeStyle = '#47474f';
-        context.lineWidth = 5;
+        context.lineWidth = 2;
         context.strokeRect(CANVAS_WIDTH / 6, CANVAS_HEIGHT / 12, CANVAS_WIDTH / 1.5, CANVAS_HEIGHT / 4);
 
         context.fillStyle = '#121212';
-        // context.lineWidth = 5;
         context.fillRect(CANVAS_WIDTH / 6, CANVAS_HEIGHT / 2.5, CANVAS_WIDTH / 1.5, CANVAS_HEIGHT / 2.5);
 
         context.strokeStyle = '#47474f';
-        // context.lineWidth = 5;
         context.strokeRect(CANVAS_WIDTH / 6, CANVAS_HEIGHT / 2.5, CANVAS_WIDTH / 1.5, CANVAS_HEIGHT / 2.5);
-        
-    
+
+        // Smoothly transition the title color
+        this.titleColor = this.lerpColor(this.titleColor, this.targetColor, this.colorTransitionSpeed);
+        const titleColorString = `rgb(${Math.round(this.titleColor.r)}, ${Math.round(this.titleColor.g)}, ${Math.round(this.titleColor.b)})`;
+
         context.font = '50px Alagard';
-        context.fillStyle = 'Red';
+        context.fillStyle = titleColorString; // Use the current title color
         context.textAlign = 'center';
         context.fillText('ViKing', CANVAS_WIDTH / 2, CANVAS_HEIGHT / 4);
 
         context.textAlign = 'left';
-
         context.font = '25px Alagard';
+
+        // Render shadow effect by duplicating the text
         this.menuOptions.forEach((option, index) => {
+            // Render shadow
+            if (this.blinking && this.currentSelection === index) {
+                context.fillStyle = this.blinkState ? 'black' : 'transparent';
+            } else {
+                context.fillStyle = 'black';
+            }
+            context.fillText(option, CANVAS_WIDTH / 5 + 5, CANVAS_HEIGHT / 2 + index * 40 + 5);
+
+            // Render actual text
             if (this.blinking && this.currentSelection === index) {
                 context.fillStyle = this.blinkState ? 'yellow' : 'black';
             } else {
