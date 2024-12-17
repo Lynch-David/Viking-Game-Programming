f35e62d7d84a2e3fe6d59516ba628213be0e392b
David Lynch <davidlynch1919@gmail.com>
2024-12-17 17:19:57 -0500

    Updated image path for Viking.tsx

--- src/ViKing.tsx
+++ src/ViKing.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.0" name="ViKing" tilewidth="16" tileheight="16" tilecount="1024" columns="32">
- <image source="C:/Users/david/Downloads/Textures-16.png" width="512" height="512"/>
+<tileset version="1.10" tiledversion="1.11.0" name="Viking" tilewidth="16" tileheight="16" tilecount="1024" columns="32">
+ <image source="../assets/images/Textures-16.png" width="512" height="512"/>
 </tileset>
