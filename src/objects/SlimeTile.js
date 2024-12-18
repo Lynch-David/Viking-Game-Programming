a1f27506bd5c249ea7c81ccdf0134aeba8759a30
Lucas Lalumière <lucas.lalum@gmail.com>
2024-12-18 06:59:41 -0500

    added cooldown for bounce sound

--- src/objects/SlimeTile.js
+++ src/objects/SlimeTile.js
@@ -4,13 +4,22 @@ import Tile from "./Tile.js";
 import SoundName from "../enums/SoundName.js";
 
 export default class SlimeTile extends Tile {
-	/**
-	 * @param {Player} player - The player currently interacting with.
-	 */
-	static applyBehavior(player) {
-		sounds.play(SoundName.slimeBounce);
-		player.isSticky = false;
-		player.isBouncing = true;
-		player.isSliding = false; 
-	}
+    static lastSoundTime = 0; // Static variable to track the last time the sound was played
+
+    /**
+     * @param {Player} player - The player currently interacting with.
+     */
+    static applyBehavior(player) {
+        const currentTime = Date.now();
+        const cooldown = 500; // 1 second cooldown
+
+        if (currentTime - SlimeTile.lastSoundTime >= cooldown) {
+            sounds.play(SoundName.slimeBounce);
+            SlimeTile.lastSoundTime = currentTime;
+        }
+
+        player.isSticky = false;
+        player.isBouncing = true;
+        player.isSliding = false;
+    }
 }
